3b8ec0a6eccf6550fe1c5d43130a91ae8f337d10
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-17 20:10:16 +0100

    learning

--- dayThree.py
+++ dayThree.py
@@ -1,1 +1,10 @@
 # Controle flow and logical Operators:
+# if else statements
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm ?\n"))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("no you cant ride")
---
+++ oddOrEven.py
@@ -0,0 +1,7 @@
+print("Give me a whole number and I will tell you if its odd or even")
+number = int(input("Whats your number? : "))
+
+if number % 2 == 0:
+    print("This is even")
+else:
+    print("this is odd")
