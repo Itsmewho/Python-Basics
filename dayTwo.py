45bbb3be244c7dab6de8a01b87752a96c7488e71
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-17 08:23:31 +0100

    dya tweo

---
+++ dayTwo.py
@@ -0,0 +1,12 @@
+# data types:
+    #str
+    #int
+    #bool
+    #float
+
+#strings:
+
+print("hello world"[::-1]) # reverse a string
+print("hello world"[0]) # to print the first number
+print("hellow world"[:-1]) # to print the last one
+print("hello world"[1::2]) # to print some of it play around with this :P
