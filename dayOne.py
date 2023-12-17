ef9929fe554210c80ab9cc6669f333db81bda650
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-17 07:21:02 +0100

    basicssssssssss

---
+++ dayOne.py
@@ -0,0 +1,11 @@
+# print("hello " + input("What is your name?"))
+# input will change when given this is kind of a blackbox just accept it i guess
+
+# print(len(input("what is your name?")))
+# the len function will give you the length of a string. 
+
+name = input("what is your name?")
+
+print("hello " + name + "!")
+
+#if you want to show your input save it a a var.
