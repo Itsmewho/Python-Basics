f6a5a7eeb5138aecd37255584a945d12478f68ec
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-17 07:58:50 +0100

    update

---
+++ dayOneChallange.py
@@ -0,0 +1,10 @@
+# band name yeaj
+
+# print("Hello to the bandname genarator!\n")
+# name = input("what is your name?\n")
+# print("Hello " + name)
+# city = input("What city you grew up in?\n")
+# print(city + " I always liked that city!")
+# pet = input("Whats the name of your pet?\n")
+# print(pet +" How nice!")
+# print(f"your bandname = {name}{pet}{city}")
