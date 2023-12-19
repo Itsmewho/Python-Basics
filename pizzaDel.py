56d62dbe5b98ce9d68cf7cea47f30550fe60441b
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-19 19:32:06 +0100

    okidokie

--- pizzaDel.py
+++ pizzaDel.py
@@ -1,7 +1,6 @@
 print("Thank you for choosing Python pizza Deliveries!")
 size = input("What size pizza do you want? s,m or l ?\n")
 
-
 bill = 0
 
 if size == "s":
@@ -12,20 +11,20 @@ if size == "l":
     bill += 25
 elif size == "":
     print("No choice is made")
-print(f"You bill is ${bill}")
+print(f"Your bill is ${bill}")
 
 add_pepperoni = input("Do you want pepperoni?   y or n ?\n")
 if size == "s" and add_pepperoni == "y":
     bill += 2
 if size == "m" or size == "l" and add_pepperoni == "y":
     bill += 3
-elif add_pepperoni == " " or add_pepperoni == "n":
-    print(f"No pepperoni will be added. Your bill is still ${bill}")
+if add_pepperoni == "" or add_pepperoni == "n":
+    print("No pepperoni will be added.")
 print(f"your bill is ${bill}")
 
 extra_cheese = input("Do you want extra cheese? y or n ?\n")
 if extra_cheese == "y":
     bill += 1
-elif extra_cheese == "" or extra_cheese == "n":
-    print(f"your final bill is ${bill}")
+if extra_cheese == "" or extra_cheese == "n":
+    print("No cheese will be added.")
 print(f"your final bill is ${bill}")
