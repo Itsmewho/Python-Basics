1caefcddc3bff57bb9031741aea92eea4f8ffec8
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-23 09:07:52 +0100

    better then yesterday

---
+++ auction/secretAuction.py
@@ -0,0 +1,43 @@
+import sys
+
+sys.path.append("auction")
+
+import os
+
+clear = lambda: os.system("cls")
+
+from art import logo
+
+print(logo)
+print("\n Wolcome to the auction \n")
+
+
+bids = {}
+end_auction = False
+
+
+def find_highest(record):
+    highst_amount = 0
+    name = ""
+    for bidder in record:
+        bid_amount = record[bidder]
+        if bid_amount > highst_amount:
+            highst_amount = bid_amount
+            name = bidder
+    print(f"The highest bidder is: {name}, with a bid of: {amount} ")
+
+# make a while loop for the auction
+
+while end_auction != True:
+    name = input("What is your name?")
+    amount = int(input("What is the amount you want to bid?"))
+    bids[name] = amount
+    while True:
+        response = input("Any more bidders? Type in (y or n)").lower()
+        if response.startswith("y"):
+            clear()
+            break #Don't forget this one :P
+        if response.startswith("n"):
+            find_highest(bids)
+            end_auction = True
+            break
--- ceasar/main.py
+++ ceasar/main.py
@@ -32,3 +32,4 @@ while not should_end:
     if restart == "no":
         should_end = True
         print("Goodbye")
+ 
