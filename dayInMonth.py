20591c57b845c188a2f676af03a5cdeddcc584ce
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-24 17:06:56 +0100

    think so?

---
+++ dayInMonth.py
@@ -0,0 +1,5 @@
+# get user input with day and year. 
+# add to  it a dict
+# make a function
+# make the loop.
+# check days in month Check for leap year for Februari.
---
+++ functionWoutput.py
@@ -0,0 +1,16 @@
+# AKA the return funciton
+
+def format_name(f_name, l_name):
+    name = f_name.title()
+    return f"{name}"
+name = format_name("jordy","jordy")
+
+#For now it seems to overcomplicate things
+#But, for bigger projects you can write functions, place them in an other folder, with the structure:
+# -main.py
+#   -childeren.py
+#       -silbings.py
+#           -granny.py ?
+# Guess here come's the practicality of OOP in place.
+
+print(name)
