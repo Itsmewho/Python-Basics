4e8c635e43d85167b75ad6f76ea9210b430446de
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2023-12-24 20:33:01 +0100

    next time:

---
+++ CapstoneBJ/blackjack.py
@@ -0,0 +1,17 @@
+# Create a simple version of bj
+
+# make the list for de cards
+# make some art for print numbers
+
+# make a function for the logic like 1+1=2 and 
+# make a function for check if number is higher then 21 game ends with result
+# this function olso check if <17 then pick an other cards
+# let this function keep adding till player types in yes or no
+# if player puts in wrong input ask again till right answer is given
+# make a function for the computer(let it play like human?)
+# show only its first card
+# if computer is bust player wins else:
+# make a function Compare results to check whos the winner, loser, draw.
+
+# Use random module for picking cards[keep in mind A's = 1 or 11 depending if player is > 21, k,q,j are 10]
+# Don't to the plit decks. Dont overthink or complicate it.
