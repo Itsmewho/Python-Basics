0d723050f9d95bfd92654255d267e206a8ed2651
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2024-01-02 14:08:42 +0100

    functions

---
+++ Books/moreFunctions.py
@@ -0,0 +1,47 @@
+# # More about functions.
+# # These has a lot more to then than you know
+
+
+# def greet():
+#     print("Hallo")
+
+
+# greet()
+
+
+# def greed_user(username):
+#     print(f"Hello {username.title()}")
+
+
+# greed_user("jordy")
+
+# #User 2 different params
+
+# def greed_user_1(animal,person):
+#     print(f"{animal}")
+#     print(f"{person}")
+# greed_user_1('dear','jordy')
+
+
+# def get_name(first, last):
+#     full_name = f"{first} {last}"
+#     return full_name
+
+# name = get_name("jim", "hendrink")
+
+# print(name)
+
+#returning a dict
+
+# def build(firs,last, age=None):
+#     person = {'firstname':'first','lastname':'last'}
+#     if age:
+#         person['age'] = age
+#         return person   
+
+# muse = build("jimy","hendrik")
+
+
+# print(muse)
+
+
