fa1d792cd410bb7e97181a9cfc85262c3622a164
Itsmewho <57646192+Itsmewho@users.noreply.github.com>
2024-01-03 18:27:14 +0100

    classes?

---
+++ introClasses.py
@@ -0,0 +1,23 @@
+# class User:
+#     pass
+
+
+# user_1 = User()
+# user_1.id = "001"
+# user_1.name = "jordy"
+
+
+# print(user_1.name)
+
+# class User:
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.username = username
+
+# user_1 = User("001", "jack")
+# user_2 = User("002", "jordy")
+# user_3 = User("003","Ballo")
+
+# print(user_1.username)
+# print(user_1.id)
+# print(user_3.id, user_3.username)
